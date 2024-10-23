af6aeef6b09d8c1d798b8b6d0558edecf63e76ff
es_python <espython85@gmail.com>
2024-10-23 09:42:31 +0300

    Feat: Add config module and setup user module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,12 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(
+      process.env.MONGODB_URI || 'mongodb://localhost:27017/esygenDB',
+    ),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '../..', 'client', 'dist'),
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
